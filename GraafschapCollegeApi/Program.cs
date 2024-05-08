ff6afb918798f79111ed9974c2114178b03a7bba
Yannick <yannick.huet@student.graafschapcollege.nl>
2024-05-08 09:17:23 +0200

    third commit

--- GraafschapCollegeApi/Program.cs
+++ GraafschapCollegeApi/Program.cs
@@ -14,6 +14,22 @@ namespace GraafschapCollegeApi
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)),
+            ClockSkew = TimeSpan.Zero
+        };
+    });
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
